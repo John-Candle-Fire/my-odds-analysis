db68efaaa4200bbcf11a97ff50b6c155a6692a22
John-Candle-Fire <johnc_auyeung@yahoo.com>
2025-08-18 10:37:06 +0800

    2025-08-18 add users

--- src/auth/authData.js
+++ src/auth/authData.js
@@ -1,15 +1,39 @@
-// Auto-generated auth data (2025-05-06T18:27:31.664Z)
+// Auto-generated auth data (2025-08-18T02:23:11.829Z)
 const AUTH_DATA = {
   "admin": {
-    "hash": "$2b$10$ymMhCMRUBDyi1lKsl0wdaOGf2ore4BlVdWFWQfW4XGIKhHwcTTlA2",
+    "hash": "$2b$10$c11EtXSESbynedDL5JvNQuWq2z6ZgZIwZZ2CEJb2qeXmLD5TZ/Xca",
     "role": "admin"
   },
   "yoko": {
-    "hash": "$2b$10$Cl.vE1kLtNzcFSTviHaX8eswfkvj/vOPilOZV.zQcvxUcau57mI5.",
+    "hash": "$2b$10$K6iLnChjteDaXsjN.Z2mCOVrF3LYxSpqnd0o4XCoRBMNk3hsdfaeG",
     "role": "user"
   },
   "hbn": {
-    "hash": "$2b$10$zH0nkheXpAIjZJ6oIoP3HeuYdi.pTYRiZDBGmhUOImstGjsH3YVcy",
+    "hash": "$2b$10$P28gN0XRV9i1wweo12IdNuW/2fMU/jyGsT54toHgvXhJqVWVhvv0S",
+    "role": "user"
+  },
+  "pfsa": {
+    "hash": "$2b$10$sctkET4t71gRoDXWW/xEF.vhm1MboQ/cft/PNpgymqNJdmduAGTcy",
+    "role": "user"
+  },
+  "isl": {
+    "hash": "$2b$10$VYhK4XCkJqA989omJwpFlu3r7CfekBg20avJIVGikXY.BlRKVJqxe",
+    "role": "user"
+  },
+  "charles": {
+    "hash": "$2b$10$OBLr16JgV47S1xcZKUG5LuwR8ZkUy9UyVwXjtwxrzpFUao1aa3Vh6",
+    "role": "user"
+  },
+  "rod": {
+    "hash": "$2b$10$RD/SVlfvNyWNthVB3Ud6TegMkO8gVO7WoZwUxV06NUHUbCKMj2dYy",
+    "role": "user"
+  },
+  "angus": {
+    "hash": "$2b$10$E2T9/h0cicRfafsIkCh7r.aJlmz3cdN7OaafX.m/k6e1F0HwCnzny",
+    "role": "user"
+  },
+  "william": {
+    "hash": "$2b$10$Q2UMvv17r0O5QTb.lTsEme8Dwhwj90HhW21jtfrxvIJXbzsbVmGF.",
     "role": "user"
   }
 };
